0425a7ba10fbbbf37ba0e261c0c46000ed0736d7
danielSoler93 <daniel.soler@e-campus.uab.cat>
2023-05-17 08:20:26 +0200

    feat: create third view to connect, request, and sign

---
+++ examples/with-create-react-app/src/views/thirdView.tsx
@@ -0,0 +1,54 @@
+import { ConnectButton } from 'diditsdktest';
+import { useSignMessage } from 'wagmi';
+import { useState } from 'react';
+import React from 'react';
+
+function ThirdView() {
+  const [policy, setPolicy] = useState('');
+  const { signMessageAsync } = useSignMessage({});
+
+  return (
+    <>
+      <ConnectButton
+        label="Connect Wallet"
+        accountStatus="address"
+        chainStatus="full"
+      />
+      <button
+        onClick={async () => {
+          const requestBody = JSON.parse(
+            JSON.stringify(
+              {
+                scope: 'openid',
+                wallet_address: '0xE993117440fE22506cF91ea386970cf84D5f45dE',
+                claims:
+                  'read:name write:name read:email write:email read:picture write:picture',
+              },
+              null,
+              2
+            )
+          );
+          const response = await fetch(
+            'https://apx.dev.gamium.world/avatar/auth/wallet_authorization',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(requestBody),
+            }
+          );
+          const data = await response.json();
+          setPolicy(data.policy);
+        }}
+      >
+        send request
+      </button>
+      <button onClick={() => signMessageAsync({ message: 'hola' })}>
+        sign
+      </button>
+    </>
+  );
+}
+
+export default ThirdView;
