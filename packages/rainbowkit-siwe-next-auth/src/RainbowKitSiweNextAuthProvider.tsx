0ce80f3e4f1dc476e102320c14eafc99e0b76ef8
danielSoler93 <daniel.soler@e-campus.uab.cat>
2023-05-24 08:52:31 +0200

    feat: logout if expired token

--- packages/rainbowkit-siwe-next-auth/src/RainbowKitSiweNextAuthProvider.tsx
+++ packages/rainbowkit-siwe-next-auth/src/RainbowKitSiweNextAuthProvider.tsx
@@ -156,8 +156,28 @@ export function DiditProvider({
 function getLocalStorage() {
   const token = window.localStorage.getItem(`_gamium_token_`);
   if (token) {
+    const token_info = parseJwt(token);
+    if (token_info.exp * 1000 < Date.now()) {
+      return false;
+    }
     return token;
   } else {
     return false;
   }
+}
+
+function parseJwt(token: string) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
 }
